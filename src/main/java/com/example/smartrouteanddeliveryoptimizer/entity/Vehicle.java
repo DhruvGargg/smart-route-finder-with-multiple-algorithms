cdfbabf423f546730165f6ae0990f85a7db8ebc9
DhruvGargg <dhruv.2327cseai1185@kiet.edu>
2026-08-29 23:25:03 +0530

    Vehicle Choices Class Added

---
+++ src/main/java/com/example/smartrouteanddeliveryoptimizer/entity/Vehicle.java
@@ -0,0 +1,4 @@
+package com.example.smartrouteanddeliveryoptimizer.entity;
+
+public class Vehicle {
+}
---
+++ src/main/java/com/example/smartrouteanddeliveryoptimizer/repository/VehicleRepository.java
@@ -0,0 +1,4 @@
+package com.example.smartrouteanddeliveryoptimizer.repository;
+
+public class VehicleRepository {
+}
